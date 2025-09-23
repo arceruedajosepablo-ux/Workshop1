a9f13ce35ab4b76f991f3bf01ebfb666344d6863
arceruedajosepablo-ux <arceruedajosepablo@gmail.com>
2025-09-23 17:59:04 -0600

    Update fecha_hora.php

--- fecha_hora.php
+++ fecha_hora.php
@@ -1,4 +1,18 @@
 <?php
-date_default_timezone_set("America/Costa_Rica");
-echo "Fecha y hora actuales: " . date("Y-m-d H:i:s");
+date_default_timezone_set("America/Mexico_City");
+$fechaHora = date("Y-m-d H:i:s");
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Fecha y Hora Actual</title>
+</head>
+<body>
+    <h1>Fecha y hora actuales:</h1>
+    <p><?php echo $fechaHora; ?></p>
+
+    <a href="index.html">Volver</a>
+</body>
+</html>
